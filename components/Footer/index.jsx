a56c4600214f06a5fbe6f5612639ffcddf46fb1d
conmebol-jzarate <jzarate@conmebol.com>
2024-07-23 16:42:35 -0400

    se agrega footer

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,20 +1,40 @@
+import Link from "next/link"
+import Body from "../Body/Body"
+import FacebookIcon from "../Icons/FacebookIcon"
+import InstagramIcon from "../Icons/InstagramIcon"
+import XIcon from "../Icons/XIcon"
+import YoutubeIcon from "../Icons/YoutubeIcon"
+
 export default function Footer() {
   return (
-    <section className="w-full">
-      <div className="mr-auto ml-auto sm:px-6 max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-4xl xl:px-0">
-        <div>
-          <p className="text-2xl font-bold">
-            {'¿Te gustaría recibir más información?'}
-          </p>
+    <Body>
+      <div className="sm:flex sm:items-center">
+        <div className="logo flex-1 pb-8">
+          <Link href="/">
+            <p className="text-2xl">
+              {'Next JS - Blog'}
+            </p>
+          </Link>
         </div>
-        <div className="pt-4 pb-4">
-          <form className="flex flex-col">
-            <input className="mb-4" type="text" name="Nombre" placeholder="Nombre" />
-            <input className="mb-4"type="text" name="Correo Electronico" placeholder="Correo Electronico" />
-            <button className="btn-primary" type="submit">Suscribirme</button>
-          </form>
+        <div className="text-center pb-8">
+          <p className="text-sm border-b-4 border-green-300">
+            {'created by'}
+          </p>
+          <Link href="https://github.com/juanbjs" target="_blank">
+            <p className="text-base">
+              {'Juan Zarate'}
+            </p>
+          </Link>
+        </div>
+        <div className="rrss flex-1 pb-8">
+          <div className="flex justify-end space-x-2">
+            <FacebookIcon href={'https://www.facebook.com/'} />
+            <InstagramIcon href={'https://www.instagram.com/'} />
+            <XIcon href={'https://x.com/'} />
+            <YoutubeIcon href={'https://www.youtube.com/'} />
+          </div>
         </div>
       </div>
-    </section>
+    </Body>
   )
 }
